cbc172b81df93e26a02de6b014bf89f0b2d30c53
Patrick Rodrigues <47316946+RodPaDev@users.noreply.github.com>
2020-09-11 17:09:33 +0100

    initial commit

---
+++ lesson01.py
@@ -0,0 +1,24 @@
+## Estou aprender a programar 
+
+## Data Types:
+
+## int : numeros inteiros
+## float : numeros, com virgula a ponto preciso
+## string : um conjunto de carateis 
+## bool : um estado de condicao: true or false
+## null : nada (em python é none)
+## undefined : nao definido
+
+
+num = 12
+floaty = 3.69
+string = "foo"
+booly = False
+nada = None
+
+
+## functions
+
+def bar():
+    print(string)
+    
